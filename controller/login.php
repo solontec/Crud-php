000460bddd0895c57a458ed430b3c5f558750475
Guilherme Solon de Menezes <guisolon.tec@gmail.com>
2025-08-05 08:16:14 -0300

    Update login.php

--- controller/login.php
+++ controller/login.php
@@ -4,17 +4,19 @@ require_once "../model/conn.php";
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
 
-    $email = $_POST['email'] ?? '';
-    $senhaRaw = $_POST['senha'] ?? '';
+    $email = filter_var($_POST['email'] ?? '', FILTER_SANITIZE_EMAIL); // o filter var pode validar os emails e o seu formato, para que na hora do cadastro não chegue emails em outros formatos
+    $senha = htmlspecialchars($_POST['senha'] ?? '', ENT_QUOTES, 'UTF-8') ; // usado para defesa de XSS, que basicamente vamos controlar os dados recebidos para não receber scripts maliciosos
 
-    if(empty($email) || empty($senhaRaw)){ // utilizando empty para verificar se o campo ( que a pessoa inseriu está vazio)
+    if(!filter_var($email, FILTER_VALIDATE EMAIL)){
+       echo "email inválido" ;
+
+    if(empty($email) || empty($senha)){ // utilizando empty para verificar se o campo ( que a pessoa inseriu está vazio)
         echo "preencha todos os campos"; // EMPTY -----
-        exit;
-    }
+        exit();
+    } 
 
     
-
-    $verificar = "SELECT id, email FROM usuarios WHERE email = ? "; // verificar o email para casos de duplicado
+    $verificar = "SELECT id, email FROM usuarios WHERE email = '$email' "; // verificar o email para casos de duplicado
     $stmt = $conn->prepare($verificar); // fzd o prepare!
  
     if(!$stmt){
@@ -22,29 +24,26 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     } // caso o prepare nao aconetça da maneira correta ele da erro
 
     $stmt->bind_param("s", $email); // bind_param para linkar os parametros
-    
+    $stmt->execute();
+
+        
     $result = $stmt->get_result(); // armazenar dentro dessa variavel o que você pegou do email que a pessoa colocou
 
-        $stmt->execute(); // executar sql
     if($result->num_rows > 0){ // o num_rows ele percorre na variavel que guarda o resultado, caso o mesmo valor for encontrado, > 0, então ele fala q já cadastrou o email
         echo "email cadasrado";
-        $stmt->close(); // fecha o prepare
-        $conn->close(); // fecha a conexao com banco
         exit(); // o exit é usado para caso a situação aconteça ele da um break igual switch case
     }
 
     
-    $senha = password_hash($senhaRaw, PASSWORD_DEFAULT); // gerando o hash da senha novamente, passando a outra variavel e divindo codigo
-
+    $senhaHash = password_hash($senha, PASSWORD_DEFAULT); // gerando o hash da senha novamente, passando a outra variavel e divindo codigo
     $cadastro = "INSERT INTO usuarios(email, senha) VALUES (?, ?)"; // insere no banco com insert nos campos 
-
     $stmt = $conn->prepare($cadastro);  // prepara ja o cadsatro novamente
 
     if(!isset($stmt)){
         echo "erro na preparação"; // verificação para prepare()
     }
 
-    $stmt->bind_param("ss", $email, $senha); // usa o bind_param nos parametros corretos, para linkar com os parametros
+    $stmt->bind_param("ss", $email, $senhaHash); // usa o bind_param nos parametros corretos, para linkar com os parametros
     
 
     if($stmt->execute()){ // se a query der certo e o execute inserir corretamente ele vai cadsatrar
